ec478ae5b593f97e9b3f15e47e64a3464c1c5317
soenneker <jake@soenneker.com>
2026-08-29 14:50:21 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Close.HttpClients/CloseOpenApiHttpClient.cs
+++ src/Soenneker.Close.HttpClients/CloseOpenApiHttpClient.cs
@@ -11,7 +11,7 @@ using Soenneker.Utils.HttpClientCache.Abstract;
 
 namespace Soenneker.Close.HttpClients;
 
-///<inheritdoc cref="ICloseOpenApiHttpClient"/>
+/// <inheritdoc cref="ICloseOpenApiHttpClient"/>
 public sealed class CloseOpenApiHttpClient : ICloseOpenApiHttpClient
 {
     private readonly IHttpClientCache _httpClientCache;
